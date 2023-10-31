f40f7bc6bb7b7db4d903dc08447719665d3ff182
simseulnyang <happysseul627@gmail.com>
2023-11-01 03:45:00 +0900

    :hammer:Chore : restaurant_recommendation 오타 수정 #3

--- src/restaurant_recommendation/restraunts/apps.py
+++ src/restaurant_recommendation/restraunts/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RestrauntsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "restraunt_recommendation.restraunts"
+    name = "restaurant_recommendation.restraunts"
