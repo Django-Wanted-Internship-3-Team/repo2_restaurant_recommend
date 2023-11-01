4428532237d805497b9dd16a1ee92cf84b38f68e
chestnut90 <cowzon90@gmail.com>
2023-11-02 07:33:18 +0900

    :recycle:Refact : Review model 리팩터 	#16
    - rating의 default=0 및 0~5 constraints 추가
    - content의 length 255 및 default="" 추가
    
    Related to #16

--- restaurants_recommendation/reviews/models.py
+++ restaurants_recommendation/reviews/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.db.models import Q
 
 
 class Review(models.Model):
-    rating = models.FloatField(null=True)
-    content = models.CharField(max_length=128)
+    rating = models.FloatField(default=0)
+    content = models.CharField(max_length=255, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     restaurant = models.ForeignKey("restaurants.Restaurant", on_delete=models.SET_NULL, null=True)
@@ -11,6 +12,9 @@ class Review(models.Model):
 
     class Meta:
         db_table = "reviews"
+        constraints = [
+            models.CheckConstraint(check=(Q(rating__gte=0) & Q(rating__lte=5)), name="rating_0_to_5_constraint"),
+        ]
 
     def __str__(self):
         return f"[{self.restaurant.business_name} - {self.user.username}]: {self.content}"
