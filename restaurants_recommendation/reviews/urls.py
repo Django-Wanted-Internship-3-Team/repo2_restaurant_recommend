bfe712a7b98cb5a2148a7f5a10c2fb92e67b742f
chestnut90 <cowzon90@gmail.com>
2023-11-02 15:05:44 +0900

    :recycle:Refact : mypy type check

--- restaurants_recommendation/reviews/urls.py
+++ restaurants_recommendation/reviews/urls.py
@@ -1,9 +1,9 @@
-from typing import Callable, List
+from typing import List
 
-from django.urls import path
+from django.urls import URLPattern, path
 
 from restaurants_recommendation.reviews.views import ReviewAPIView
 
-urlpatterns: List[Callable] = [
+urlpatterns: List[URLPattern] = [
     path(r"", ReviewAPIView.as_view(), name="reviews"),
 ]
