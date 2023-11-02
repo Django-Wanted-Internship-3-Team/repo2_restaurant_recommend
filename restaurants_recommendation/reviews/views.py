191b1ec14c127dc583880fbda94cd166f6c22dbd
Sa Potato <wogur981208@gmail.com>
2023-11-03 01:13:21 +0900

    add_feature-019: 맛집 상세정보 고도화(API) 작성 (#31)
    * :white_check_mark:Test: 맛집 상세정보 고도화(API) 테스트 코드 작성 #19
    
    Related to #19
    
    * :sparkles:Feat: 맛집 상세정보 고도화(API) 작성 #19
    
    - 5개 이상의 평가가 존재하는(=인기있는)맛집만 캐싱
    Related to #19
    
    * :recycle:Refact: 맛집 상세 정보 조회의 리뷰 10개만 조회 수정 #19
    
    Related to #19

--- restaurants_recommendation/reviews/views.py
+++ restaurants_recommendation/reviews/views.py
@@ -10,14 +10,10 @@ from restaurants_recommendation.reviews.serializers import ReviewSerializer
 
 
 class ReviewAPIView(APIView):
-    """
-    Review api view
-    """
-
     permission_classes = [IsAuthenticated]
 
     @swagger_auto_schema(
-        operation_summary="add review into restaurant.",
+        operation_summary="식당 리뷰 등록",
         request_body=ReviewSerializer,
         responses={
             status.HTTP_201_CREATED: ReviewSerializer,
@@ -27,7 +23,16 @@ class ReviewAPIView(APIView):
     )
     def post(self, request: Request):
         """
-        식당에 리뷰를 등록하는 api
+        식당에 리뷰를 등록합니다.
+
+        Returns:
+            id (int): id
+            restaurant (int): 식당 id
+            user (int): 유저 id
+            content (str): 리뷰 내용
+            rating (int): 평점
+            created_at (datetime): 생성일
+            updated_at (datetime): 수정일
         """
         serializer = ReviewSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
