9ace35db0aa3e498827d265f336c57e84c6df27a
simseulnyang <happysseul627@gmail.com>
2023-11-02 23:25:52 +0900

    :green-heart: CI: CI TEST

--- restaurants_recommendation/users/serializers.py
+++ restaurants_recommendation/users/serializers.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from django.contrib.auth import authenticate
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
