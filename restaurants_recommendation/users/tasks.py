b578ad4c883b58aa5b5670d0af06420fc0899f13
chestnut90 <cowzon90@gmail.com>
2023-11-07 07:33:01 +0900

    :recycle:Refact : 식당 추천 기능의 webhook 데이터 전달 코드 추가 및 수정 #20
    Related to #20

--- restaurants_recommendation/users/tasks.py
+++ restaurants_recommendation/users/tasks.py
@@ -1,3 +1,4 @@
+import requests
 from celery import shared_task
 
 from restaurants_recommendation.common.utils import lat_lon_to_km
@@ -27,9 +28,29 @@ def recommend_restaurants(user: User, distance: float = 0.5, count: int = 5):
         return []
 
 
+def post_webhook(user: User):
+    base_url = "http://localhost:8000/api/restaurants"
+
+    data = {
+        "username": "LunchHere",
+        "content": f"Hello {user.username}, Your LunchHere!",
+        "embeds": [
+            {
+                "author": {
+                    "name": f"{restaurant.business_name}",
+                    "url": f"{base_url}/{restaurant.id}",
+                },
+            }
+            for restaurant in recommend_restaurants(user)
+        ],
+    }
+
+    requests.post(user.webhook, json=data)
+
+
 @shared_task
 def recommend_restaurants_to_user():
     users = User.objects.filter(is_lunch_recommend=True)
 
     for user in users:
-        recommend_restaurants(user)
+        post_webhook(user)
--- restaurants_recommendation/users/tests/test_recommend_restaurants.py
+++ restaurants_recommendation/users/tests/test_recommend_restaurants.py
@@ -42,6 +42,51 @@ class RecommendRestaurantsTestCase(TestCase):
     def test_recommend_restaurant_scheduler(self, mock_now):
         mock_now.return_value = datetime(2023, 11, 6, 12, 0, 0)  # mon
 
+        User.objects.create(
+            username="user",
+            latitude="40.7132",
+            longitude="-70.0060",
+            is_lunch_recommend=True,
+            webhook="https://discord.com/api/webhooks/1169806248393314334/40Xh7yJvX5yvBQQEHK0PmogVEX7EyeoapZ7wAB63qmcwPOnML-Qm-282mEVMBx6ljdZu",
+        )
+
+        Restaurant.objects.create(
+            restaurant_code="1",
+            business_name="서브웨이",
+            latitude="40.7132",
+            longitude="-70.0060",
+            rating=0,
+        )
+
+        Restaurant.objects.create(
+            restaurant_code="2",
+            business_name="짬뽕나라",
+            latitude="40.8132",
+            longitude="-70.0060",
+            rating=5,
+        )
+        Restaurant.objects.create(
+            restaurant_code="3",
+            business_name="맥도날드",
+            latitude="40.71325",
+            longitude="-70.0060",
+            rating=5,
+        )
+        Restaurant.objects.create(
+            restaurant_code="4",
+            business_name="만리장성",
+            latitude="40.71324",
+            longitude="-70.0060",
+            rating=5,
+        )
+        Restaurant.objects.create(
+            restaurant_code="5",
+            business_name="다온",
+            latitude="40.71326",
+            longitude="-70.0060",
+            rating=5,
+        )
+
         result = recommend_restaurants_to_user.apply()
         self.assertTrue(result.successful())
 
